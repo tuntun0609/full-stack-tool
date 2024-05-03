fcee10f16369cd06c8ce85f24fe42e9c918e36b5
tuntun <760523867@qq.com>
2024-05-03 09:27:27 +0800

    fix: mobile overlay

--- src/components/header/mobile-menu.tsx
+++ src/components/header/mobile-menu.tsx
@@ -4,14 +4,9 @@ import { cn } from '@/lib/utils'
 import { Menu as MenuIcon } from 'lucide-react'
 import { useRef, useState } from 'react'
 import { Menu } from '../sidebar/menu'
-import { useClickAway } from 'react-use'
 
 export const MobileMenu = () => {
   const [menuOpen, setMenuOpen] = useState(false)
-  const sidebarRef = useRef<HTMLDivElement>(null)
-  useClickAway(sidebarRef, () => {
-    setMenuOpen(false)
-  })
   return (
     <>
       <div
@@ -25,9 +20,13 @@ export const MobileMenu = () => {
         className={cn(
           'fixed z-100 w-screen h-screen bg-gray-600 opacity-20 top-0 left-0 transition-all duration-300 ease-in-out',
           !menuOpen && 'opacity-0 pointer-events-none'
-        )}></div>
+        )}
+        onClick={() => {
+          if (menuOpen) {
+            setMenuOpen(false)
+          }
+        }}></div>
       <div
-        ref={sidebarRef}
         className={cn(
           'fixed inset-0 z-20 mx-0 h-screen w-60 -translate-x-60 bg-background transition-transform duration-300 ease-in-out',
           menuOpen && 'translate-x-0'
